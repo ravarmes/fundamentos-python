c0909836e3918f3590a2f4c72a012aa5d6781681
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2021-09-09 13:24:13 -0300

    Versao 01

--- 05-sintaxe-comentarios.py
+++ 05-sintaxe-comentarios.py
@@ -3,7 +3,7 @@ X = 10 #Este é um comentário de linha e será ignorado
 
 '''A seção de comentários multilinhas começou, 
 marcada por 3 aspas simples. Como resultado, toda esta seção 
-de comentários será ignorada pelo intérprete. 
+de comentários será ignorada pelo interpretador. 
 A seguinte declaração com 3 aspas simples 
 finaliza este bloco de comentário. '''
 
