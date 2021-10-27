cc33ff4a61bda7fb7ec52452bbb83c6294c9316a
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2021-10-27 14:55:07 -0300

    Repetição aninhada com for

---
+++ 13-comandosrepeticaoaninhados-for.py
@@ -0,0 +1,9 @@
+#Demonstração do Laços For's Aninhados
+
+for linha in range (1, 11): 
+    for coluna in range (1, 11):
+        #Imprimi linha x coluna com tabulação no final 
+        print ( linha * coluna, end="\t" ) 
+    #Para saltar linha
+    print() 
+
